4ee7329665fddc50876f6678204fd5befcac5668
Gabriel Gutierrez <ggutierreztamayo@gmail.com>
2024-11-22 22:14:04 -0500

    Modify solution of problem F of the ICPC Latin America Regional 2024

--- ICPC Latin America Regionals/2024/F.cpp
+++ ICPC Latin America Regionals/2024/F.cpp
@@ -5,24 +5,15 @@ using namespace std;
 int main(){
 	ios_base::sync_with_stdio(0);cin.tie(NULL);
 	int k, n; cin>>k>>n;
-	int aux = n;
-	if(((n+2)/3) <= k && k <= ((n+1)/2)){
-		string ans = "";
-		while(aux > 0){
-			k--;
-			if(((aux-2+2)/3) <= k && k <= ((aux-2+1)/2)){
-				aux -= 2;
-				ans += "X-";
-			}
-			else{
-				aux -= 3;
-				ans += "-X-";
-			}
-		}
-		while(ans.size() > n)
-		ans.pop_back();
-		cout<<ans<<endl;
-	}
-	else
-	cout<<"*"<<endl;
+	int minN = 2*k - 1;
+	int r3 = min(k, n - minN);
+	int r2 = k - r3;
+	
+	string ans = "";
+	for(int i=0; i<r3; i++) ans += "-X-";
+	for(int i=0; i<r2; i++) ans += "X-";
+	if(ans.size() > n) ans.pop_back();
+	
+	if(ans.size() == n) cout<<ans<<endl;
+	else cout<<"*"<<endl;
 }
